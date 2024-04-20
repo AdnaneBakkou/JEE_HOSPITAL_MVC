1c661fcd38fc0a6c9364f522db94e157edf8ce6f
adnane bakkou <bakkouadnane88@gmail.com>
2024-04-20 16:24:29 +0100

    fin video 2 jdbc auth

--- src/main/java/ma/bakkou/hospitalmvcp1/HospitalMvcP1Application.java
+++ src/main/java/ma/bakkou/hospitalmvcp1/HospitalMvcP1Application.java
@@ -7,8 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.util.Date;
 
@@ -30,6 +33,29 @@ public class HospitalMvcP1Application implements CommandLineRunner {
         //    patientRepository.save(new Patient(null,"meryem",new Date(),true,102));
 
 
+    }
+
+   // @Bean
+CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+        PasswordEncoder passwordEncoder=passwordEncoder();
+
+        return args -> {
+            UserDetails u1= jdbcUserDetailsManager.loadUserByUsername("user11");
+     if (u1==null)  jdbcUserDetailsManager.createUser(
+                User.withUsername("user11").password(passwordEncoder.encode("12345678")).roles("USER").build()
+        );
+            UserDetails u2= jdbcUserDetailsManager.loadUserByUsername("user22");
+
+            if (u2==null)   jdbcUserDetailsManager.createUser(
+                    User.withUsername("user22").password(passwordEncoder.encode("12345678")).roles("ADMIN").build()
+            );
+            UserDetails u3= jdbcUserDetailsManager.loadUserByUsername("user33");
+
+            if (u3==null)  jdbcUserDetailsManager.createUser(
+                    User.withUsername("user33").password(passwordEncoder.encode("12345678")).roles("USER","ADMIN").build()
+            );
+
+        };
     }
 
     @Bean
--- src/main/java/ma/bakkou/hospitalmvcp1/security/SecurityConfig.java
+++ src/main/java/ma/bakkou/hospitalmvcp1/security/SecurityConfig.java
@@ -13,7 +13,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -23,9 +26,15 @@ public class SecurityConfig {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
-
 @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource)
+    {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+
+//@Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
         return  new InMemoryUserDetailsManager(
                 User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
@@ -37,7 +46,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .formLogin(form -> form
-                        .loginPage("/login") // Set the custom login page
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")   // Set the custom login page
                         .permitAll() // Permit everyone to see the login page. Don't require authentication.
                 )
                 .authorizeHttpRequests(ar->ar.requestMatchers("/webjars/**").permitAll())
